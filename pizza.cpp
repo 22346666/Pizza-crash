a94820a05c426d679c80bcf91f80a6eff4c2929b
Nikita Tkachenko <mercartar@gmail.com>
2023-12-23 16:57:32 +0200

    Working on db query for pizza

--- pizza.cpp
+++ pizza.cpp
@@ -5,5 +5,9 @@
 
 QJsonObject Pizza::get_json(const QSqlDatabase &db)
 {
+    QSqlQuery query(db);
+    query.exec(QString("SELECT * FROM users WHERE UserEmail = '%1'"));
+    if(!query.next()){
+    }
     return QJsonObject();
 }
